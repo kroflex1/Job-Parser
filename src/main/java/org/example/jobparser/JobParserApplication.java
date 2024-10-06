82be4b9350c46411ca6e37263b49c3ea890e5131
Artyom Sokolov <15012003artem@mail.ru>
2024-10-06 20:37:41 +0500

    initial  commit

---
+++ src/main/java/org/example/jobparser/JobParserApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jobparser/JobParserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
